cc35bfe075a6eb61240988971d8de06514090d7a
bujosa <davidbujosa@gmail.com>
2021-01-02 03:27:49 -0400

    Added all modules import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 import { YellowModule } from './yellow/yellow.module';
 
 @Module({
-  imports: [YellowModule],
+  imports: [
+    YellowModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      context: ({ req }) => ({ headers: req.headers }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: `./`,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [],
   providers: [],
 })
