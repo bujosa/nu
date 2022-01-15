662280e1660b6181f38a0029830802d97131e7e2
bujosa <davidbujosa@gmail.com>
2022-01-15 22:05:50 +0100

    Eliminated unused variables

--- src/house/house.resolver.ts
+++ src/house/house.resolver.ts
@@ -5,30 +5,30 @@ import { UpdateHouseInput } from './graphql/inputs/update-house-input';
 import { House } from './graphql/types/house.type';
 import { HouseService } from './house.service';
 
-@Resolver((of) => House)
+@Resolver(() => House)
 export class HouseResolver {
   constructor(private houseService: HouseService) {}
 
-  @Query((returns) => House)
+  @Query(() => House)
   public async getHouseById(
     @Args('input') getHouseByIdInput: GetHouseByIdInput,
   ): Promise<House> {
     return this.houseService.getHouseById(getHouseByIdInput);
   }
 
-  @Mutation((of) => House)
+  @Mutation(() => House)
   public async createHouse(
     @Args('input') createHouseInput: CreateHouseInput,
   ): Promise<House> {
     return this.houseService.createHouse(createHouseInput);
   }
 
-  @Query((returns) => [House])
+  @Query(() => [House])
   public async getAllHouses(): Promise<House[]> {
     return this.houseService.getAllHouses();
   }
 
-  @Mutation((of) => House)
+  @Mutation(() => House)
   public async updateHouse(
     @Args('input') updateHouseInput: UpdateHouseInput,
   ): Promise<House> {
