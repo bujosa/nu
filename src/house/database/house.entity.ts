8546083ea24d3a81bbd694156e1081dcba84f60a
bujosa <davidbujosa@gmail.com>
2022-01-15 22:10:55 +0100

    Changes type to color

--- src/house/database/house.entity.ts
+++ src/house/database/house.entity.ts
@@ -22,7 +22,7 @@ export class House extends Document implements IBaseEntity, IHouse {
     ref: Color.name,
     autopopulate: { maxDepth: 2 },
   })
-  color: MongooseSchema.Types.ObjectId;
+  color: Color;
 
   @Prop({ required: true })
   createdAt: string;
